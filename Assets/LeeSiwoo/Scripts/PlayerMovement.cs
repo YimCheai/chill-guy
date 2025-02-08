c30a40ae10f0b51291b103729f9ade4cc61ad41b
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-02-09 00:59:58 +0900

    Stage 1, Save Effect Added
    스테이지 1, 세이브 효과 추가

--- Assets/LeeSiwoo/Scripts/PlayerMovement.cs
+++ Assets/LeeSiwoo/Scripts/PlayerMovement.cs
@@ -8,6 +8,14 @@ public class PlayerMovement : MonoBehaviour
     private MicInput micInput;
     [SerializeField]
     private CinemachineVirtualCamera cinemachine;
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private GameObject SaveEffect;
+
+    private string PointName;
+
+    private Vector3 RespawnPos;
 
     private Animator animator;
 
@@ -23,19 +31,18 @@ public class PlayerMovement : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (GameDone) return;
-        InputHandler();
-    }
-    void InputHandler()
+	private void FixedUpdate()
+	{
+		if (GameDone) return;
+		InputHandler();
+	}
+	void InputHandler()
     {
         float decibel = micInput.GetDecibel(_ref);
         if (decibel >= 7 && decibel < 15)
         {
             animator.SetBool("isMove", true);
-            rigid.AddForce(Vector3.right * 1.1f, ForceMode2D.Force); return;
+            rigid.velocity = new Vector2(Speed, rigid.velocity.y); return;
         }
         else if(decibel >= 15 &&  decibel < 20 && !isJump)
         {
@@ -57,10 +64,30 @@ public class PlayerMovement : MonoBehaviour
             isJump = false;
         }
 	}
-    public void Death()
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.CompareTag("SavePoint"))
+        {
+            RespawnPos = collision.gameObject.transform.position;
+            if(PointName != collision.gameObject.name)
+            {
+                GameObject clone = Instantiate(SaveEffect, RespawnPos, Quaternion.identity);
+                Destroy(clone, 0.3f);
+                PointName = collision.gameObject.name;
+            }
+        }
+	}
+	public void Death()
     {
         Debug.Log("Death");
         GameDone = true;
         cinemachine.Follow = null;
+        Invoke("Respawn", 0.7f);
+    }
+    private void Respawn()
+    {
+        GameDone = false;
+        cinemachine.Follow = this.gameObject.transform;
+        transform.position = RespawnPos;
     }
 }
